94e2d44c326fff5b93cb5026671746b621f78008
Dinesh Shettenor - C014 - Code Ctrl Solutions Private Limited <129173689+Dineshshettenor-c014-codectrlsolutions@users.noreply.github.com>
2023-12-02 14:19:36 +0530

    worked on Learners are viewing

---
+++ main.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    autoplay:{
+        delay:5000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+
+  
