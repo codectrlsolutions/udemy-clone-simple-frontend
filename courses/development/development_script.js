148385e665a6580d09da538300c45855445103b6
Dinesh Shettenor - C014 - Code Ctrl Solutions Private Limited <129173689+Dineshshettenor-c014-codectrlsolutions@users.noreply.github.com>
2023-12-13 19:01:50 +0530

    1.started working on courses/categories page (in progress).    - started working on Courses to get you started    - Started working on Featured courses.

---
+++ courses/development/development_script.js
@@ -0,0 +1,69 @@
+showMostPopular();
+
+// showMostPopular() 
+function showMostPopular() {
+    document.getElementById('most_popular_container').style.display = "block";
+    document.getElementById('new_container').style.display = "none";
+    document.getElementById('trending_container').style.display = "none";
+
+    document.getElementById('most_popular').style.cursor = "default";
+    document.getElementById('new').style.cursor = "pointer";
+    document.getElementById('trending').style.cursor = "pointer";
+
+    document.getElementById('most_popular').style.borderBottom = "2px solid black";
+    document.getElementById('new').style.borderBottom = "none";
+    document.getElementById('trending').style.borderBottom = "none";
+}
+
+// showNew() 
+function showNew() {
+    document.getElementById('most_popular_container').style.display = "none";
+    document.getElementById('new_container').style.display = "block";
+    document.getElementById('trending_container').style.display = "none";
+
+    document.getElementById('most_popular').style.cursor = "pointer";
+    document.getElementById('new').style.cursor = "default";
+    document.getElementById('trending').style.cursor = "pointer";
+
+    document.getElementById('most_popular').style.borderBottom = "none";
+    document.getElementById('new').style.borderBottom = "2px solid black";
+    document.getElementById('trending').style.borderBottom = "none";
+}
+
+// showTrending() 
+function showTrending() {
+    document.getElementById('most_popular_container').style.display = "none";
+    document.getElementById('new_container').style.display = "none";
+    document.getElementById('trending_container').style.display = "block";
+
+    document.getElementById('most_popular').style.cursor = "pointer";
+    document.getElementById('new').style.cursor = "pointer";
+    document.getElementById('trending').style.cursor = "default";
+
+    document.getElementById('most_popular').style.borderBottom = "none";
+    document.getElementById('new').style.borderBottom = "none";
+    document.getElementById('trending').style.borderBottom = "2px solid black";
+}
+
+const badge_text = document.querySelectorAll('.course-badge');
+
+// Step 2: Iterate through the selected paragraphs
+badge_text.forEach(paragraph => {
+    // Step 3: assigning each paragraph's text content
+    const current_text = paragraph.textContent;
+
+    // Step 4: Modify each paragraph based on the text
+    if (current_text == 'Best seller') {
+      paragraph.style.backgroundColor = '#eceb98'; 
+    } else if (current_text == 'Highest rated') {
+      paragraph.style.backgroundColor = '#f3ca8c'; 
+    } else if(current_text == 'Hot & new'){
+      paragraph.style.backgroundColor = '#fcbca0'; 
+    }
+    else if(current_text == 'Hot & new'){
+        paragraph.style.backgroundColor = '#fcbca0'; 
+      }
+      else{
+        paragraph.style.display = 'none';
+      }
+  });
