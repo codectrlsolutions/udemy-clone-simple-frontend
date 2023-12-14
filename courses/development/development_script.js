3cb21059f9a5d2d241ffa787383cefc29f9ef808
Dinesh Shettenor - C014 - Code Ctrl Solutions Private Limited <129173689+Dineshshettenor-c014-codectrlsolutions@users.noreply.github.com>
2023-12-14 18:59:25 +0530

    1.started working on courses/categories page (in progress).    - worked on Featured courses.    - worked on Popular topics.    - worked on Popular Instructors.

--- courses/development/development_script.js
+++ courses/development/development_script.js
@@ -67,3 +67,21 @@ badge_text.forEach(paragraph => {
         paragraph.style.display = 'none';
       }
   });
+
+  const swiper = new Swiper('.swiper',
+  {
+    // Optional parameters
+    // direction: 'vertical',
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
